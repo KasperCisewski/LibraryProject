f9f455a6e4de68b40b0168ebd768d3c90b28b470
Kasper Cisewski <kaspercisewski@gmail.com>
2018-05-21 14:32:06 +0200

    add tests

--- LibraryProjectTest/UnitTest1.cs
+++ LibraryProjectTest/UnitTest1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using LibraryProject.Builders;
 using LibraryProject.Model;
 using LibraryProject.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Linq;
 
 namespace LibraryProjectTest
 {
@@ -30,7 +32,7 @@ namespace LibraryProjectTest
 
 
         [TestMethod]
-        public void FirstTestBookValidate()
+        public void FirstBookISBNNumberValidatorTest()
         {
             Init();
             BookValidator bookValidator = new BookValidator();
@@ -39,7 +41,7 @@ namespace LibraryProjectTest
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void SecondTestBookValidate()
+        public void SecondBookISBNNumberValidatorTest()
         {
             Init();
             bool expected = false;
@@ -47,6 +49,115 @@ namespace LibraryProjectTest
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void FirstIsBookNameInLibraryTest()
+        {
+            BookValidator bookValidator = new BookValidator();
+            bool expected = true;
+            string bookName = "Star book";
+            List<Book> books = new List<Book>
+            {
+                new Book("Book about nothing","Richards","9994443332221"),
+                new Book("Star book","galikowski","1234567891111"),
+                new Book("Lord of the rings","tolkien","1111222233334")
+            };
+            var actual = bookValidator.IsBookNameInBooks(bookName, books);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void SecondIsBookNameInLibraryTest()
+        {
+            BookValidator bookValidator = new BookValidator();
+            bool expected = false;
+            string bookName = "Star book";
+            List<Book> books = new List<Book>
+            {
+                new Book("Book about nothing","Richards","9994443332221"),
+                new Book("Lord of the rings","tolkien","1111222233334")
+            };
+            var actual = bookValidator.IsBookNameInBooks(bookName, books);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void FirstIsBookInAvailableBooksTest()
+        {
+            BookValidator bookValidator = new BookValidator();
+            bool expected = true;
+            string bookName = "Star book";
+            List<Book> books = new List<Book>
+            {
+                new Book("Book about nothing","Richards","9994443332221"),
+                new Book("Star book","galikowski","1234567891111"),
+                new Book("Lord of the rings","tolkien","1111222233334")
+            };
+
+            var actual = bookValidator.IsBookNameInAvailableBooks(bookName, books);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void SecondIsBookInAvailableBooksTest()
+        {
+
+            BookValidator bookValidator = new BookValidator();
+            bool expected = false;
+            string bookName = "Star book";
+            List<Book> books = new List<Book>
+            {
+                new Book("Book about nothing","Richards","9994443332221"),
+                new Book("Star book","galikowski","1234567891111"),
+                new Book("Lord of the rings","tolkien","1111222233334")
+            };
+            books.First(x => (x.BookName == bookName));
+            foreach(var item in books.Where(x=>(x.BookName==bookName)))
+            {
+                item.IdPerson = 1;
+            }
+
+            var actual = bookValidator.IsBookNameInAvailableBooks(bookName, books);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FirstIsPersonInPeopleListTest()
+        {
+            string firstName = "kasper";
+            string lastName = "cisewski";
+            var expected = true;
+            PersonValidator personValidator = new PersonValidator();
+            List<Person> peopleList = new List<Person>
+            {
+                new Person(0, "tomek", "baranski"),
+                new Person(1,"Marcin","czapiewski"),
+                new Person(2,"kasper","cisewski")
+            };
+            var actual = personValidator.IsPersonInList(firstName, lastName, peopleList);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SecondIsPersonInPeopleListTest()
+        {
+            string firstName = "kasper";
+            string lastName = "cisewski";
+            var expected = false;
+            PersonValidator personValidator = new PersonValidator();
+            List<Person> peopleList = new List<Person>
+            {
+                new Person(0, "tomek", "baranski"),
+                new Person(1,"Marcin","czapiewski")
+            };
+            var actual = personValidator.IsPersonInList(firstName, lastName, peopleList);
+
+            Assert.AreEqual(expected, actual);
+        }
+
 
         [TestMethod]
         public void FirstBorrowedBookBuilderTest()
@@ -65,11 +176,17 @@ namespace LibraryProjectTest
             var person = new Person(0, "kasper", "cisewski");
             var book = new Book("znak", "gulczynski", "1234567891234");
             person.BorrowedBooks.Add(book);
+
             BorrowedBookListBuilder borrowedBookListBuilder = new BorrowedBookListBuilder();
             var expected = "znak - gulczynski - 1234567891234\r\n";
             var actual = borrowedBookListBuilder.BuildBorrowedBookList(person);
 
             Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FirstBookInLibrary()
+        {
+           
         }
     }
 }
